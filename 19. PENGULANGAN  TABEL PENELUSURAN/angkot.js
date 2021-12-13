14b7310dd76e5c471f64705963e0fc451b896c5c
ibayscode <muhamadiqbaladityaputra@gmail.com>
2021-12-13 22:39:22 +0700

    Menambahkan folder 19

---
+++ 19. PENGULANGAN  TABEL PENELUSURAN/angkot.js
@@ -0,0 +1,8 @@
+// Program Aplikasi Jurangan Angkot Sederhana
+var noAngkot = 1;
+var jmlAngkot = 10;
+
+while (noAngkot <= jmlAngkot) {
+  console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
+  noAngkot++;
+}
