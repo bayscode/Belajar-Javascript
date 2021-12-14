b0bef068eac846f9fb77db3ce81c6723ba865194
ibayscode <muhamadiqbaladityaputra@gmail.com>
2021-12-15 00:56:13 +0700

    Menambahkan folder 21

---
+++ 21. PENGKONDISIAN IF ELSE pada JAVASCRIPT/angkot.js
@@ -0,0 +1,14 @@
+// Aplikasi Juragan Angkot
+
+var noAngkot = 1,
+  angkotBeroperasi = 6,
+  jmlAngkot = 10;
+
+// For
+for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
+  if (noAngkot <= angkotBeroperasi) {
+    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
+  } else {
+    console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi.');
+  }
+}
