52ce2dd2f952be201b828d699ea239f7caf9be1f
ibayscode <muhamadiqbaladityaputra@gmail.com>
2021-12-19 16:45:25 +0700

    Menambahkan folder 28

---
+++ 28. FUNCTION pada JAVASCRIPT  Membuat dan Menjalankan FUNCTION/volumeKubus.js
@@ -0,0 +1,14 @@
+// Case menghitung volume 2 buah kubus.
+// 1. Ketahui sisi masing" kubus
+var kubusA = 8;
+var kubusB = 3;
+var volumeA;
+var volumeB;
+var total;
+// 2. Hitung volume masing" kubus.
+volumeA = kubusA * kubusA * kubusA;
+volumeB = kubusB * kubusB * kubusB;
+// 3. Jumlahkan hasilnya
+total = volumeA + volumeB;
+// 4. Kembalikan nilai jawabannya
+console.log(total);
