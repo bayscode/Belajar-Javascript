c74c8874612a6bb100356d275057af473bdb1127
ibayscode <muhamadiqbaladityaputra@gmail.com>
2021-12-22 11:33:28 +0700

    Menambahkan folder 33

---
+++ 33. FUNCTION pada JAVASCRIPT DECLARATION vs. EXPRESSION/alertFunction.js
@@ -0,0 +1,10 @@
+function tampilPesan(nama) {
+  alert('Halo ' + nama);
+}
+tampilPesan('Iqbal'); // Function Declaration
+
+
+var tampilPesan = function (nama) {
+  alert('Halo ' + nama);
+}
+tampilPesan('Iqbal'); // Function Expression
