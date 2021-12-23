96066a17abe21ee15de1d56734d4aa03c1b9ab95
ibayscode <muhamadiqbaladityaputra@gmail.com>
2021-12-23 10:17:54 +0700

    Menambahkan folder 36

---
+++ 36. ARRAY pada JAVASCRIPT SLICE dan SPLICE/slice.js
@@ -0,0 +1,5 @@
+// Slice
+// slice(awal,akhir);
+var kruOnePiece = ['Luffy','Zoro','Nami','Usopp','Sanji','Chopper','Robin','Franky','Brook','Jimbei'];
+var kruNew = kruOnePiece.slice(7,10);
+console.log(kruNew.join(' '));
---
+++ 36. ARRAY pada JAVASCRIPT SLICE dan SPLICE/splice.js
@@ -0,0 +1,6 @@
+var myArr = ['Muhamad', 'Iqbal', 'Putra', 'Luffy', 'Zoro', 'Nami', ' Chopper'];
+
+// Splice
+// splice(Index awal, mauDihapusBerapa, elemenBaru1, elemenBaru2, n ...)
+myArr.splice(1,1);
+console.log(myArr.join(' '));
