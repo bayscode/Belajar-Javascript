41802b0b0a8d4468222cc19097a88dfede034a15
ibayscode <muhamadiqbaladityaputra@gmail.com>
2021-12-27 00:19:11 +0700

    Menambahkan folder 40

--- 40. MEMBUAT PROGRAM PENGELOLAAN PENUMPANG ANGKOT dengan JAVASCRIPT (Bagian 2)/angkot.js
+++ 40. MEMBUAT PROGRAM PENGELOLAAN PENUMPANG ANGKOT dengan JAVASCRIPT (Bagian 2)/angkot.js
@@ -1,4 +1,5 @@
-var penumpang = ['Iqbal', undefined, 'Aditya'];
+// Function Tambah Penumpang
+var penumpang = [];
 var tambahPenumpang = function (namaPenumpang, penumpang) {
   //Jika Angkot Kosong
   if (penumpang.length == 0) {
@@ -31,6 +32,36 @@ var tambahPenumpang = function (namaPenumpang, penumpang) {
         // Kembalikkan isi array dan keluar dari function
         return penumpang;
       }
+    }
+  }
+}
+
+// Function Hapus Penumpang
+var hapusPenumpang = function (namaPenumpang, penumpang) {
+  // Jika angkot Kosong
+  if (penumpang.length == 0) {
+    // Tampilkan pesan bahwa angkot kosong, dan tidak mungkin ada penumpang turun
+    console.log('Angkot masih kosong!');
+    // Kembalikan isi array dan keluar dari return 
+    return penumpang;
+  }
+  // Selain
+  else {
+    // Telusuri seluruh kursi dari awal
+    for (var i = 0; i < penumpang.length; i++) {
+      // Jika penumpang sesuai hapus penumpang dengan mengubah namanya, menjadi Undefined
+      if (penumpang[i] == namaPenumpang) {
+        penumpang[i] = undefined;
+        // Kembalikan isi array dan keluar dari return
+        return penumpang;
+      }
+      // Jika tidak ada nama yang sesuai
+      else if (i == penumpang.length - 1) {
+        // Tampilkan pesan kesalahannya
+        console.log(namaPenumpang + ' tidak ada didalam angkot!');
+      }
+      // Kembalikan isi array dan keluar dari return
+      return penumpang;
     }
   }
 }
